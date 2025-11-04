bfba500a334bdd456a65292e384f5d9706318be5
lupor01 <rlupor01@student.bbk.ac>
2025-11-04 14:02:43 +0000

    defined TinyConvAutoEncoder

--- 1 practising /architectures.py
+++ 1 practising /architectures.py
@@ -1,11 +1,13 @@
 import torch
 from torch import nn
 
-class TiniestAutoEncoder(nn.Module):
-
-
+class TinyAutoEncoder(nn.Module):
+    """
+    Super simple auto encoder architecture for flat in/out configurations.
+    
+    Args: self-explanatory✨
+    """
     def __init__(self, input, latent, nonlinearity=nn.ReLU):
-        super().__init__()
         self._input = input
         self._latent = latent
         self._nonl = nonlinearity
@@ -24,4 +26,32 @@ class TiniestAutoEncoder(nn.Module):
         output = self.encoder(data)
         output = self.decoder(output)
         return output
-    
+
+
+class TinyConvAutoEncoder(nn.Module):
+    """
+    Super simple convolutional auto encoder for image input/output.
+    """
+    def __init__(self, channels=1, nonlinearity=nn.ReLU):
+        super().__init__()
+        self._channels = channels
+        self._nonl = nonlinearity
+
+        self.encoder = nn.Sequential(
+            nn.Conv2d(self._channels, 10, kernel_size=3, stride=2, padding=1),
+            self._nonl(),
+            nn.Conv2d(10, 20, kernel_size=3, stride=2, padding=1),
+            self._nonl()
+        )
+
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(20, 10, kernel_size=3, stride=2, padding=1, output_padding=1),
+            self._nonl(),
+            nn.ConvTranspose2d(10, self._channels, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, data):
+        output = self.encoder(data)
+        output = self.decoder(output)
+        return output
--- 1 practising /simple_auto_mnist.py
+++ 1 practising /simple_auto_mnist.py
@@ -34,6 +34,77 @@ dataloader_test = DataLoader(mnist_test, batch_size=30, shuffle=True)
 #     print(X.shape)
 #     break
 
+# model = TiniestAutoEncoder(input=784, latent=50)  #MNIST images (28, 28) –> flatten!!!
+# loss_fn = nn.MSELoss()
+# optimiser = torch.optim.Adam(model.parameters(), lr=0.01)
+
+# epochs = 10
+# noise = 0.4
+# losses = []
+# for e in range(epochs):
+#     loss_sum = 0
+#     i = 0
+#     for i, (X, _) in enumerate(dataloader_train):
+#         X = X.reshape((-1, 784))  #MNIST images (28, 28) –> flatten!!!
+#         noisy = X + noise * torch.randn_like(X)
+
+#         optimiser.zero_grad()
+#         pred = model(noisy)
+#         loss = loss_fn(pred, X)
+#         loss.backward()
+#         optimiser.step()
+#         loss_sum += loss.item()
+
+#     losses.append(loss_sum/(i+1))
+#     print(f"Avg.loss at {e}th epoch: {loss_sum/(i+1)}")
+
+
+# model.eval()
+# with torch.no_grad():
+#     tot_loss = 0
+#     for i, (X, _) in enumerate(dataloader_test):
+#         X = X.reshape((-1, 784))
+#         noisy = X + noise * torch.randn_like(X)
+#         pred = model(noisy)
+#         loss = loss_fn(pred, X)
+#         tot_loss += loss.item()
+    
+#     print(f"\nAvg. test loss: {tot_loss/(i+1)}")
+
+#     # test_subset = Subset(mnist_test, indices=range(5))
+#     # small_loader = DataLoader(test_subset, batch_size=5)
+#     # for X, _ in small_loader:
+#     #     X = X.reshape((-1, 784))
+#     #     pred = model(X)
+#     #     # print(pred.shape)
+
+
+#     test_subset = Subset(mnist_test, indices=range(5))
+#     loader_subset = DataLoader(test_subset, batch_size=5)
+#     for X, _ in loader_subset:
+#         X = X.reshape((-1, 784))
+#         noisy = X + noise * torch.randn_like(X)
+#         pred = model(noisy)
+#         pred = pred.view(-1, 1, 28, 28)  #does not changes elements in memory!!!
+
+#         fig, axes = plt.subplots(3, 5, figsize=(7, 7))
+
+#         for i in range(5):
+#             axes[0, i].imshow(X[i].view(28, 28), cmap="gray")
+#             axes[0, i].axis("off")
+#             axes[0, 1].set_title(f"original")
+
+#             axes[1, i].imshow(noisy[i].view(28, 28), cmap="gray")
+#             axes[1, i].axis("off")
+#             axes[1, i].set_title(f"noisy")
+
+#             axes[2, i].imshow(pred[i].view(28, 28), cmap="gray")
+#             axes[2, i].axis("off")
+#             axes[2, i].set_title(f"predictions")
+        
+#         plt.show()
+
+
 model = TiniestAutoEncoder(input=784, latent=50)  #MNIST images (28, 28) –> flatten!!!
 loss_fn = nn.MSELoss()
 optimiser = torch.optim.Adam(model.parameters(), lr=0.01)
@@ -70,13 +141,6 @@ with torch.no_grad():
         tot_loss += loss.item()
     
     print(f"\nAvg. test loss: {tot_loss/(i+1)}")
-
-    # test_subset = Subset(mnist_test, indices=range(5))
-    # small_loader = DataLoader(test_subset, batch_size=5)
-    # for X, _ in small_loader:
-    #     X = X.reshape((-1, 784))
-    #     pred = model(X)
-    #     # print(pred.shape)
 
 
     test_subset = Subset(mnist_test, indices=range(5))
