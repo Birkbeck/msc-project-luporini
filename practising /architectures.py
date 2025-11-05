6e89e35ae7f3c581c9725acaf6dc26b86c4e63b4
lupor01 <rlupor01@student.bbk.ac>
2025-11-05 12:54:53 +0000

    TinyConvClassifier: first draft

--- practising /architectures.py
+++ practising /architectures.py
@@ -59,13 +59,14 @@ class TinyConvAE(nn.Module):
 
 class FlexyConvAE(nn.Module):
     """
-    Based on TinyConvAE but adding stride arg
+    Auto encode based on TinyConvAE but adding stride arg
 
-    input_shape: e.g., MNIST == (1, 28, 28); CIFAR10 == (3, 32, 32)
-    stride: stride value.. the same for each convolutional layer
-    padding: padding pixels
-    kernel: filter size
-    nonlinearity: activation function after convLayers
+    Args:
+        input_shape: e.g., MNIST == (1, 28, 28); CIFAR10 == (3, 32, 32)
+        stride: stride value.. the same for each convolutional layer
+        padding: padding pixels
+        kernel: filter size
+        nonlinearity: activation function after convLayers
     """
     def __init__(self, input_shape=(1, 28, 28), stride=2, padding=1, kernel=3, nonlinearity=nn.ReLU):
         super().__init__()
@@ -102,3 +103,48 @@ class FlexyConvAE(nn.Module):
         output = self.decoder(output)
         return output
     
+
+class TinyConvClassifier(nn.Module):
+    """
+    Classifier based on FlexyConvAE..
+
+    Args:
+        input_shape: e.g., MNIST == (1, 28, 28); CIFAR10 == (3, 32, 32)
+        stride: stride value.. the same for each convolutional layer
+        padding: padding pixels
+        kernel: filter size
+        nonlinearity: activation function after convLayers
+        classes: number of classes (problem/dataset dependent)
+    """
+    def __init__(self, input_shape=(1, 28, 28), stride=2, padding=1, kernel=3, nonlinearity=nn.ReLU, classes=10):
+        super().__init__()
+        self._input = input_shape[1]
+        self._channels = input_shape[0]
+        self._stride = stride
+        self._pad = padding
+        self._kernel = kernel
+        self._nonl = nonlinearity
+        self._classes = classes
+
+        self.encoder = nn.Sequential(
+            nn.Conv2d(self._channels, 10, kernel_size=self._kernel, stride=self._stride, padding=self._pad),
+            self._nonl(),
+            nn.Conv2d(10, 20, kernel_size=self._kernel, stride=self._stride, padding=self._pad),
+            self._nonl() #using nn.ReLU might push values out of 0,1 interval?? NOT RELEVANT FOR CLASSIFIERS!?
+        )
+
+        # get the input to nn.Linear() using a fake forward pass
+        # remember!!! flatten = channels * height * width 
+        fake_data = torch.zeros(1, self._channels, self._input, self._input)
+        fake_output = self.encoder(fake_data)
+        fake_shape = fake_output.shape
+        flat = fake_shape[1]*fake_shape[2]*fake_shape[3]
+        self.classifier_head = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(flat, self._classes)
+        )
+
+    def forward(self, data):
+        output = self.encoder(data)
+        output = self.classifier_head(output)
+        return output
