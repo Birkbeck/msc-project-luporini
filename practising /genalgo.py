102420f2dbbbb4c86dbbf08ac3a3bb93b20ce808
lupor01 <rlupor01@student.bbk.ac>
2025-11-12 14:06:36 +0000

    defined mutate

--- practising /genalgo.py
+++ practising /genalgo.py
@@ -23,12 +23,29 @@ def remodel(flat, model):
             index += n
     return model
 
+
+def mutate(guy:torch.Tensor, m_chance=0.05, mode="small", m_rate=0.3) -> torch.Tensor:
+    """
+    randomly mutate parameters in a 1D tensor
+
+    args:
+        m_chance: chance of mutation in "small" mode - mutatate if rand below m_chance
+        mode: either "50/50", where half of the genes mutate on avg. or else, where #mutation depends on m_chance
+        m_rate: scaling factor for mutation strength
+    """
+    if mode == "50/50":
+        mask = torch.randint_like(guy, 2) # 0-1s mask.. which params are mutated
+        strength = torch.randn_like(guy) # raw mutation effect
+        # mutation = mask * noise #?? effect too great on 1s, needs scaling
+        mutation = m_rate * mask * strength
+        #⛔️half of the genes mutated on avg!!! AGGRESSIVE?
+    else:
+        mask = torch.rand_like(guy) < m_chance
+        strength = torch.randn_like(guy)
+        mutation = m_rate * mask * strength
+    return guy + mutation
+
+
+
+
 mymodel = nn.Linear(3, 2, bias=True) # model, not a tensor!!!
-
-print(list(param.shape for param in mymodel.parameters()))
-flat = flatten(mymodel)
-print(flat)
-print(flat.shape)
-
-remodelled = remodel(flat, mymodel)
-print(mymodel == remodelled)
