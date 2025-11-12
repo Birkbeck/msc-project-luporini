a249ae233a5d419cf2f49c129fa882c3f5363cac
lupor01 <rlupor01@student.bbk.ac>
2025-11-12 13:41:52 +0000

    defined remodel

--- practising /genalgo.py
+++ practising /genalgo.py
@@ -1,23 +1,34 @@
 import torch
 from torch import nn
 
-def flatten(model): # flatten each parameter into a 1D tensor and concatenate
+def flatten(model):
+    """
+     flatten each parameter into a 1D tensor and concatenate
+    """
     return torch.cat([param.view(-1) for param in model.parameters()])
     # return torch.cat([param.data.view(-1) for param in model.parameters()]) # NO!!!!
                             # BREAKS COMPUTATION GRAPH.. don't risk it
                             # might need autograd later
 
-# model = nn.Linear(3, 2, bias=True) # model, not a tensor!!!
 
-model = nn.Sequential(
-    nn.Linear(2, 3),
-    nn.Linear(3, 2)
-)
+def remodel(flat, model):
+    """
+    remap a flat model into a non flat torch.architecture
+    """
+    index = 0
+    for param in model.parameters():
+        with torch.no_grad():
+            n = param.numel()
+            param.copy_(flat[index:index+n].view_as(param))
+            index += n
+    return model
 
-# for param in model.parameters():
-#     print(param.shape)    
+mymodel = nn.Linear(3, 2, bias=True) # model, not a tensor!!!
 
-flat = flatten(model)
-
-print(flat.shape)
+print(list(param.shape for param in mymodel.parameters()))
+flat = flatten(mymodel)
 print(flat)
+print(flat.shape)
+
+remodelled = remodel(flat, mymodel)
+print(mymodel == remodelled)
